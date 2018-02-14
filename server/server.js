215231216fd5b9ad3a29af10413664fdcc376643
Petru <escu.petru@gmail.com>
2018-02-14 10:37:22 +0200

    ceva initial

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
+
+
