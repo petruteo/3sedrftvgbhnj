831089b9ed21163f312093452327b4bed7b44621
Petru <escu.petru@gmail.com>
2018-02-14 11:23:32 +0200

    dada

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
+const http = require('http');
 const express = require('express');
+
+
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
+const socketIO = require('socket.io');
 
 
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection',  (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('browser disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
 
