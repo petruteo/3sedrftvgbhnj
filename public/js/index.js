17d9adbe5e1dba17c0187a29d67ac969f57dffaa
Petru <escu.petru@gmail.com>
2018-02-15 16:37:07 +0200

    fe adaugat

--- public/js/index.js
+++ public/js/index.js
@@ -38,13 +38,15 @@ socket.on('disconnect', () => {
 jQuery('#message-form').on('submit', (e) => {
     e.preventDefault();
 
+    var messageTextBox = jQuery('[name=message]');
+
     socket.emit('newMessage', {
         from:'User',
         // text: $(":name-message")[0].val()
         // jQuery('[name-message]').val()
-        text: jQuery( '[name=message]' ).val()
+        text: messageTextBox.val()
     }, () => {
-
+        messageTextBox.val('');
     });
 });
 
@@ -53,13 +55,18 @@ locationButton.on('click', () => {
     if (!navigator.geolocation) {
         return alert('geolocation not supported by your browser');
     }
+    locationButton.attr('disabled', 'disabled').text('Waiting');
 
     navigator.geolocation.getCurrentPosition((position) => {
+        locationButton.removeAttr('disabled').text('Send location');
         console.log(position);
         socket.emit('newLocationMessage', position.coords.latitude, position.coords.longitude);
-        
+
     },  () => {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('you did not allow location. why dude, why? :)')
     }
 )
 });
+
+// locationButton.attr('disabled', 'disabled');
